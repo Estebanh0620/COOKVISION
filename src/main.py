cf2e20d8230443d56a9e62438eb41cd8e28640ca
Estebanh0620 <est.estebanm.hurtado@unimilitar.edu.co>
2026-03-11 21:17:15 -0500

    carga de dataset
    se añade el dataset de tomate y cebolla y se empieza a escribir el main

---
+++ src/main.py
@@ -0,0 +1,62 @@
+import cv2
+import os
+
+# Ruta de la imagen de prueba
+ruta_imagen = "dataset\Tomates\Tre_001.jpg"
+
+# Cargar imagen
+imagen = cv2.imread(ruta_imagen)
+
+if imagen is None:
+    print("No se pudo cargar la imagen")
+    exit()
+
+# 1️⃣ Quitar ruido (filtro gaussiano)
+imagen_sin_ruido = cv2.GaussianBlur(imagen, (5,5), 0)
+
+# 2️⃣ Convertir a escala de grises
+gris = cv2.cvtColor(imagen_sin_ruido, cv2.COLOR_BGR2GRAY)
+
+# 3️⃣ Segmentación (umbral)
+_, umbral = cv2.threshold(gris, 120, 255, cv2.THRESH_BINARY)
+
+# 4️⃣ Detectar contornos
+bordes = cv2.Canny(gris, 50, 150)
+
+contornos, _ = cv2.findContours(
+    umbral,
+    cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE
+)
+
+# Dibujar contornos
+imagen_contornos = imagen.copy()
+
+imagen_contornos = imagen.copy()
+
+for c in contornos:
+    area = cv2.contourArea(c)
+
+    # Filtrar contornos muy pequeños o muy grandes
+    if area > 500 and area < 50000:
+        cv2.drawContours(imagen_contornos, [c], -1, (0,255,0), 2)
+
+# Crear carpeta resultados si no existe
+os.makedirs("resultados", exist_ok=True)
+
+# Guardar resultados
+cv2.imwrite("resultados/sin_ruido.jpg", imagen_sin_ruido)
+cv2.imwrite("resultados/gris.jpg", gris)
+cv2.imwrite("resultados/segmentada.jpg", umbral)
+cv2.imwrite("resultados/contornos.jpg", imagen_contornos)
+
+# Mostrar cantidad de objetos detectados
+print("Objetos detectados:", len(contornos))
+
+# Mostrar imágenes
+cv2.imshow("Original", imagen)
+cv2.imshow("Segmentada", umbral)
+cv2.imshow("Contornos", imagen_contornos)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
